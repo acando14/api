cdba51e35979c2f6914218a20ac9d1ddd9b8ef33
Ale <alexcandy91@hotmail.it>
2021-07-27 10:24:03 +0200

    api v1 hospital

--- src/Entity/Hospital.php
+++ src/Entity/Hospital.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\HospitalRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=HospitalRepository::class)
  */
 class Hospital
