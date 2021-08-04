1210dd8fac9633b82d011b2ff452d98846515315
Ale <alexcandy91@hotmail.it>
2021-08-04 14:17:23 +0200

    env test

---
+++ tests/EnvTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class EnvTest extends TestCase
+{
+    public function test_environment()
+    {
+        $this->assertTrue(
+            true
+        );
+    }
+}
